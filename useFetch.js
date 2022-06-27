e67ae143851263c9fcb8c99a316eee294b59efac
fcamposaragna <fcamposaragna@gmail.com>
2022-06-27 20:53:53 -0300

    first commit

---
+++ useFetch.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react"
+
+
+export const useFetch = ( url ) => {
+
+    const [state, setState] = useState({
+        data: null,
+        isLoading: true,
+        hasError : null,
+    });
+
+    const getfetch = async () => {
+
+        setState({
+            ...state,
+            isLoading: true
+        });
+
+        const resp = fetch(url);
+        const data = await (await resp).json();
+        
+        setState({
+            data,
+            isLoading: false,
+            hasError: null
+        })
+    }
+
+    useEffect(() => {
+        getfetch();
+    }, [url])
+    
+
+    return {
+        data: state.data,
+        isLoading: state.isLoading,
+        hasError: state.hasError
+    };
+}
